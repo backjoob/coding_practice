0c1cf4d5348464cc6e9a475b0c0d26e733cf53f2
backjoob <backjoob@back.joob>
2017-07-12 22:17:44 -0400

    comments in HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,7 +1,18 @@
 
-
+/**
+ * This class is simply a container for the static method which prints,
+ * "Hello, World!"
+ * 
+ * @author  backjoob
+ */
 class HelloWorld{
+    
+    /**
+     * Main "hello world" method.
+     * 
+     * @param args ignored
+     */
     public static void main(String[]args){
-        System.out.println("Hello, world!");
+        System.out.println("Hello, World!");
     }
 }
