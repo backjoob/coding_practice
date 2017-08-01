a3081e774bc82e2c660779050eeeca1aa7fcfb8d
backjoob <backjoob@back.joob>
2017-08-01 00:46:16 -0400

    perl hello_world

---
+++ perl/hello_world.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# these are recommended because perl allows confusing things by default
+use strict;
+use warnings;
+
+print "Hello, world!\n";
+
+# This is the same because of the newline character inside the quotes:
+#print "Hello, world!
+#";
