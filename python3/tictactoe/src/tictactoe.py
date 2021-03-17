bbb4cccba4efe446e871b4e8245bbe8e08f15156
backjoob <backjoob@back.joob>
2021-03-17 00:46:26 -0400

    TicTacToe progress

--- python3/tictactoe/src/tictactoe.py
+++ python3/tictactoe/src/tictactoe.py
@@ -2,16 +2,28 @@
 
 from array import array
 
-class InvalidMoveException(Exception):
+
+class TicTacToeException(Exception):
     """
-    Denotes that an invalid move has been attempted.
+    Parent exception for TicTacToe errors
+    """
+
+class InvalidMoveException(TicTacToeException):
+    """
+    Denotes that an invalid move has been attempted
+    """
+
+class InvalidPlayerException(TicTacToeException):
+    """
+    Denotes that a player attempted to play out of turn
     """
 
 class TicTacToe:
-    '''
+    """
     Maintains state for a Tic Tac Toe game.
     Defines mutators on state.
-    '''
+    """
+
     NUM_PLAYERS = 2
     NUM_ROWS = 3
     NUM_COLS = 3
@@ -21,34 +33,66 @@ class TicTacToe:
         return self.NUM_ROWS * self.NUM_COLS
 
     def __init__(self):
+        """Initialize a new Tic Tac Toe object"""
         # Initialize an empty board state where empty board states are represented by -1
-        self.boardstate = array('b', [-1] * (self.NUM_ROWS * self.NUM_COLS))
+        self.boardstate = array("b", [-1] * (self.NUM_ROWS * self.NUM_COLS))
         # Initialize first player
         self.current_player = 0
         # Initialize history
-        #self.history = []
+        self.history = []
 
-    def play(self, index):
-        if self.boardstate[index] == -1:
-            self.boardstate[index] = self.current_player
-            self.current_player = (self.current_player + 1) % self.NUM_PLAYERS
-        else:
+    @classmethod
+    def i_to_rc(cls, index):
+        """Convert an index to rows and cols"""
+        return (index // cls.NUM_COLS, index % cls.NUM_COLS)
+
+    @classmethod
+    def rc_to_i(cls, indices):
+        """Convert rows and cols to an index"""
+        row, column = indices
+        return row * cls.NUM_COLS + column
+
+    def play(self, player, indices):
+        """Play for `player` at position `index`.
+
+        Args:
+            player: The player attempting to make a move.
+            indices: A tuple containing a row and column.
+
+        Returns:
+            None
+
+        Raises:
+            InvalidPlayerException: It is not `player`'s turn.
+            InvalidMoveException: The space at `indices` is not empty.
+        """
+        if player != self.current_player:
+            raise InvalidPlayerException(
+                f"Attempting to move for {player}, but it is {self.current_player}'s turn."
+            )
+        index = self.rc_to_i(indices)
+        if self.boardstate[index] != -1:
             raise InvalidMoveException(
-                f"There is already a {self.boardstate[index]} in index {index}.")
+                f"There is already a {self.boardstate[index]} in position {indices}."
+            )
+        self.boardstate[index] = self.current_player
+        self.current_player = (self.current_player + 1) % self.NUM_PLAYERS
 
     def __str__(self):
-        return "\n".join("\t".join(map(str, self.boardstate[i:i+self.NUM_COLS]))
-            for i in range(0, self.num_spaces, self.NUM_COLS))
+        return "\n".join(
+            "\t".join(map(str, self.boardstate[i : i + self.NUM_COLS]))
+            for i in range(0, self.num_spaces, self.NUM_COLS)
+        )
+
 
 def main():
     t = TicTacToe()
     print(t)
     print()
     for i in range(t.num_spaces):
-        t.play(i)
+        t.play(i % 2, t.i_to_rc(i))
     print(t)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
